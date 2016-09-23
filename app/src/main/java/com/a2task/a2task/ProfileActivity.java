27a9719b3e258fcd3e84a4ad8f3ffc170db4268b
J-CITY <db4kq5b210b>
2016-09-24 08:21:46 +1000

    Короче, сделал несколько активити, посмотрите, разберитесь. Пришлось много переделать, так что сделал меньше, чем хотел, но что-то уже есть.

--- app/src/main/java/com/a2task/a2task/ProfileActivity.java
+++ app/src/main/java/com/a2task/a2task/ProfileActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
+
 import com.a2task.a2task.databases.DataBaseNote;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
